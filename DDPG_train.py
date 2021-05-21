fixed encoder normalization

DDPG_train.py
@@ -21,7 +21,7 @@ GAMMA = 0.99
 BATCH_SIZE = 64
 LEARNING_RATE = 1e-4
 REPLAY_SIZE = 80000
-REPLAY_INITIAL = 5000
+REPLAY_INITIAL = 80
 TEST_INTERV = 1000
 UNROLL = 2 
 
@@ -38,7 +38,7 @@ def test(net, ae, env, count=10, device="cpu"):
         while True: # play a full episode
             state_t = torch.tensor([state]).to(device).float()
             screen_t = torch.tensor([screen]).to(device)
-            features = torch.reshape(ae.encode(screen_t), (1,-1)).float()
+            features = torch.reshape(ae(screen_t), (1,-1)).float()
             # the reason not to use agent here is to just follow the policy
             # we don't need exploration (hence no clipping too)
             action = net(torch.column_stack((features, state_t)))[0].data.cpu().numpy()
@@ -74,7 +74,7 @@ if __name__ == "__main__":
 
     # Networks
     ae = model.Autoencoder(1, pretrained="./Autoencoder-FC.dat", device=device).to(device).float()
-    # fe = lambda x: ae.encode(x)
+
     obs_size += 64
     act_net = model.DDPGActor(obs_size, env.action_space.shape[0]).to(device).float()
     crt_net = model.DDPGCritic(obs_size, env.action_space.shape[0]).to(device).float()
@@ -89,7 +89,7 @@ if __name__ == "__main__":
     agent = common.AgentDDPG(act_net, env, buffer, ae, GAMMA, device=device, unroll_steps=UNROLL)
     act_opt = optim.Adam(act_net.parameters(), lr=LEARNING_RATE)
     crt_opt = optim.Adam(crt_net.parameters(), lr=LEARNING_RATE)
-    ae_opt = optim.Adam(ae.parameters(), lr=LEARNING_RATE)
+    # ae_opt = optim.Adam(ae.parameters(), lr=LEARNING_RATE)
     
     best_test_reward = None
     exp_count=0
@@ -109,8 +109,8 @@ if __name__ == "__main__":
                                 = buffer.sample(BATCH_SIZE)
 
             # Reduce dimensionality of the state screens
-            state_features = ae.encode(screens)
-            next_state_features = ae.encode(next_screens)
+            state_features = ae(screens)
+            next_state_features = ae(next_screens)
 
             # # Train auto encoder
             # if exp_count<50000:

lib/common.py
@@ -59,7 +59,7 @@ class AgentDDPG():
             # print("State: ",state)
             state_t = torch.tensor([state]).to(self.device).float()
             screen_t = torch.tensor([screen]).to(self.device)
-            features = torch.reshape(self.ae.encode(screen_t), (1,-1)).float()
+            features = torch.reshape(self.ae(screen_t), (1,-1)).float()
             action_t = self.act_net(torch.column_stack((features, state_t))) # actions tensor
             action = action_t[0].data.cpu().numpy()
             # action1=action.copy()
@@ -220,6 +220,7 @@ class ExperienceBuffer:
         next_screens_t = torch.tensor(np.array(next_screens, dtype=np.float32)).to(self.device)
         next_states_t = torch.tensor(np.array(next_states, dtype=np.float32)).to(self.device)
         dones_t = torch.BoolTensor(dones).to(self.device)
+        # print(torch.count_nonzero(dones_t))
 
         return screens_t, states_t, actions_t, rewards_t, dones_t, next_screens_t, next_states_t
 

lib/model.py
@@ -37,11 +37,11 @@ class Autoencoder(nn.Module):
         x = self.decode(x)
         return x
 
-    def encode(self, x): # feature extractor
+    def __call__(self, x): # feature extractor
         x = self.encoder(x)
         # x = torch.flatten(x,1)
         # x = self.en_fc(x)
-        return x
+        return torch.sigmoid(x) # to normalize the features to the same order of magniture as state
 
     def decode(self,x):
         # x = nn.functional.relu(self.de_fc(x))